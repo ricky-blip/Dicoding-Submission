0c535076f9773d07a4ca4d510a4f4fa73840684c
ricky-blip <ricky.rinaldy77@gmail.com>
2022-08-30 09:22:48 +0700

    update

--- dicoding_submission/lib/pages/setting.dart
+++ dicoding_submission/lib/pages/setting.dart
@@ -6,7 +6,100 @@ class SettingPages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Center(
+          //NOTE Column Parent
+          child: Column(
+            children: [
+              //NOTE Column 1
+              Column(
+                children: [
+                  Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                    size: 50,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Setting",
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+
+              //NOTE Column 2
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.shield,
+                        color: Colors.grey,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "General",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Text(
+                        "Profile",
+                        style: TextStyle(color: Colors.grey, fontSize: 13),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              //NOTE Column 3
+              SizedBox(height: 30),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.gpp_maybe,
+                        color: Colors.grey,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "Advanced",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Text(
+                        "Security",
+                        style: TextStyle(color: Colors.grey, fontSize: 13),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
